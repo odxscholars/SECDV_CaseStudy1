543fcde75de2ae8c223f396e4d9341ac01665b52
haroldmojica <harold_mojica@dlsu.edu.ph>
2025-08-03 20:56:47 +0800

    Added password history

--- server/database.js
+++ server/database.js
@@ -1,10 +1,29 @@
 const bcrypt = require('bcrypt');
 
 const users = [
-    { id: 1, username: 'admin', password: bcrypt.hashSync('Admin123!', 10), role: 'admin' },
-    { id: 2, username: 'manager', password: bcrypt.hashSync('Manager123!', 10), role: 'manager' },
-    { id: 3, username: 'employee', password: bcrypt.hashSync('Employee123!', 10), role: 'employee' },
+  {
+    id: 1,
+    username: 'admin',
+    password: bcrypt.hashSync('Admin123!', 10),
+    role: 'admin',
+    passwordHistory: [bcrypt.hashSync('Admin123!', 10)]
+  },
+  {
+    id: 2,
+    username: 'manager',
+    password: bcrypt.hashSync('Manager123!', 10),
+    role: 'manager',
+    passwordHistory: [bcrypt.hashSync('Manager123!', 10)]
+  },
+  {
+    id: 3,
+    username: 'employee',
+    password: bcrypt.hashSync('Employee123!', 10),
+    role: 'employee',
+    passwordHistory: [bcrypt.hashSync('Employee123!', 10)]
+  }
 ];
+
 
 // In-memory storage for blog posts
 const posts = [];
